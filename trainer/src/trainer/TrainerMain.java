4fc70537506163f9ed7f6c10ce3bb1157f9a1f7a
madacsia <74899496+madacsia@users.noreply.github.com>
2022-02-04 11:11:46 +0100

    Add files via upload

---
+++ trainer/src/trainer/TrainerMain.java
@@ -0,0 +1,30 @@
+package trainer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainerMain {
+
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            Trainer trainer = new Trainer();
+            trainer.setName("John Doe");
+            trainer.setYearOfBirth(1980);
+            System.out.println(trainer.getName());
+            System.out.println(trainer.getYearOfBirth());
+
+            Trainer anotherTrainer = new Trainer();
+            anotherTrainer.setName("Jack Doe");
+            anotherTrainer.setYearOfBirth(1981);
+            System.out.println(anotherTrainer.getName());
+
+            System.out.println(anotherTrainer.getName());
+            System.out.println(anotherTrainer.getNameAndYearOfBirth());
+
+            anotherTrainer.changeName("Joe Doe");
+            System.out.println(anotherTrainer.getName());
+
+            anotherTrainer.setName("Jack Doe");
+            System.out.println(anotherTrainer.getName());
+        }
+}
